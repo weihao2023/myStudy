f1f9730881bf78ed425cc226409846fb82e10d44
weihao <3159969031@qq.com>
2024-01-10 21:42:24 +0800

    增加指针、结构体

---
+++ 结构体/结构体/03-结构体指针.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;
+//#include <string>
+//
+////结构体指针
+//
+//struct student
+//{
+//	//成员列表
+//	string name;	//姓名
+//	int age;	//年龄
+//	int score;	//分数
+//};
+//
+//int main()
+//{
+//	//1、创建学生结构体
+//	student s = { "张三",18,100 };
+//
+//	//2、通过指针指向结构体变量
+//	student* p = &s;
+//
+//	//4、通过指针遍历结构体变量中的数据
+//	cout << "姓名：" << p->name << " 年龄：" << p->age << " 分数：" << p->score << endl;
+//	
+//	system("pause");
+//	return 0;
+//
+//}
